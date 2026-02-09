7e9bf18ebdfda29e3fb1705ef768fb7498dea872
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-09 16:53:40 -0300

    Fix urls root

--- brokercrm/urls.py
+++ brokercrm/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("CRM Fuerza Natural Brokers funcionando 🚀")
-
 
 urlpatterns = [
-    path('', include('panel.urls')),
     path('admin/', admin.site.urls),
 
-    # RUTAS DE LOGIN / LOGOUT / PASSWORD
+    # LOGIN / LOGOUT DJANGO
     path('accounts/', include('django.contrib.auth.urls')),
 
-    # HOME TEMPORAL
-    path('', home),
+    # PANEL PRINCIPAL (HOME REAL)
+    path('', include('panel.urls')),
 ]
