34b93c0136c3005200c1c890f4d2670445ebdaec
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-09 10:14:31 -0300

    Login del CRM

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # lo dejamos vacío por ahora
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
