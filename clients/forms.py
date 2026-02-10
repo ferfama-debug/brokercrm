f014f96c9081f3b18d5a84a6ef4060e73d91cb99
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-10 15:17:04 -0300

    Traduccion formulario clientes al español

--- clients/forms.py
+++ clients/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Client
 
-class ClientForm(forms.ModelForm):
+class ClienteForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = "__all__"
+
+        labels = {
+            "first_name": "Nombres",
+            "last_name": "Apellidos",
+            "phone": "Teléfono",
+            "email": "Email",
+            "producer": "Productor",
+        }
+
