a514d654f0e9126a7e2b5c6c484970f9578f76bc
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-10 15:28:02 -0300

    Fix ClientForm import error

--- clients/forms.py
+++ clients/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Client
 
-class ClienteForm(forms.ModelForm):
+class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = "__all__"
--- clients/views.py
+++ clients/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Client
 from .forms import ClientForm
-
-
-@login_required
-def lista_clientes(request):
-    clientes = Client.objects.all()
-    return render(request, "clientes/lista_clientes.html", {"clientes": clientes})
-
 
 @login_required
 def crear_cliente(request):
