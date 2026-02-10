96c759a856d78710f2ed94d42a929d37c786f277
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-10 11:23:05 -0300

    Formulario crear cliente

--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Client
+from .forms import ClienteForm
 
 
 @login_required
-def lista_clientes(request):
-    clientes = Client.objects.all()
-    return render(request, "clients/lista_clientes.html", {"clientes": clientes})
+def crear_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("lista_clientes")
+    else:
+        form = ClienteForm()
+
+    return render(request, "clientes/crear_cliente.html", {"form": form})
