92915e3f43087958e846ca5d8222874c4594cae4
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-10 13:18:33 -0300

    Rename ClienteForm to ClientForm

--- clients/forms.py
+++ clients/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Client   # ← ESTE ES EL CAMBIO IMPORTANTE
+from .models import Client
 
-class ClienteForm(forms.ModelForm):
+class ClientForm(forms.ModelForm):
     class Meta:
-        model = Client       # ← TAMBIÉN ESTE
+        model = Client
         fields = "__all__"
--- clients/views.py
+++ clients/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ClienteForm
+from .forms import ClientForm
 
 
 @login_required
 def crear_cliente(request):
     if request.method == "POST":
-        form = ClienteForm(request.POST)
+        form = ClientForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("lista_clientes")
     else:
-        form = ClienteForm()
+        form = ClientForm()
 
     return render(request, "clientes/crear_cliente.html", {"form": form})
