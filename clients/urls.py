f0724049609750bec106b9903bb94f3d51fc0f53
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-11 15:33:18 -0300

    Fix editar cliente imports

--- clients/urls.py
+++ clients/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import lista_clientes, crear_cliente
+from .views import lista_clientes, crear_cliente, editar_cliente
 
 urlpatterns = [
     path("", lista_clientes, name="lista_clientes"),
     path("nuevo/", crear_cliente, name="crear_cliente"),
     path("editar/<int:cliente_id>/", editar_cliente, name="editar_cliente"),
 ]
+
