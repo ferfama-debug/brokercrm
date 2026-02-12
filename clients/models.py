a07db7a768c42ebe6e3c4ab65abab189b6213e5b
Fuerza Natural Broker <ferfama@gmail.com>
2026-02-12 10:32:59 -0300

    Fix modelo Cliente definitivo

--- clients/models.py
+++ clients/models.py
@@ -3,17 +3,17 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
-class Client(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
     email = models.EmailField(blank=True, null=True)
-    producer = models.ForeignKey(
+    productor = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         limit_choices_to={'is_producer': True}
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    creado = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.last_name}, {self.first_name}"
+        return f"{self.apellidos}, {self.nombres}"
