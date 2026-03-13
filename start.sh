29a3dc29c17219070211d2fe34c58bc3f870a1f2
Fuerza Natural Broker <ferfama@gmail.com>
2026-03-13 18:26:00 -0300

    auto crear admin

--- start.sh
+++ start.sh
@@ -3,4 +3,14 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username='admin'
+password='admin123'
+email='admin@example.com'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+"
+
 gunicorn brokercrm.wsgi:application --bind 0.0.0.0:$PORT
