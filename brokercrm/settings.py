e207e60287e5c7bb9ee2ae38e22a359e136eeb24
Fuerza Natural Broker <ferfama@gmail.com>
2026-03-13 14:31:59 -0300

    disable alert context processor

--- brokercrm/settings.py
+++ brokercrm/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "alerts.context_processors.alert_count",
+                # "alerts.context_processors.alert_count",  # desactivado para evitar conflicto con admin
             ],
         },
     },
