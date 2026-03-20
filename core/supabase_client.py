a7d0eb2d64995235f2418efcc6c832272d75c3ce
Fabian Martinez <ferfama@icloud.com>
2026-03-20 19:51:35 -0300

    fix supabase client init

--- core/supabase_client.py
+++ core/supabase_client.py
@@ -3,24 +3,31 @@ from django.conf import settings
 import uuid
 
 
-# 🔥 CREAR CLIENTE CORRECTO
-supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+def get_supabase_client():
+    if not settings.SUPABASE_URL or not settings.SUPABASE_KEY:
+        raise Exception("Supabase no configurado correctamente")
+
+    return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
 
 def subir_archivo_supabase(file, carpeta):
     try:
+        supabase = get_supabase_client()
+
         # nombre único
         nombre_unico = f"{uuid.uuid4()}_{file.name}"
         path = f"{carpeta}/{nombre_unico}"
 
         print("SUBIENDO A SUPABASE:", path)
 
-        # 🔥 SUBIDA CORRECTA
+        # subida
         supabase.storage.from_("polizas").upload(
-            path, file.read(), {"content-type": file.content_type}
+            path,
+            file.read(),
+            {"content-type": file.content_type},
         )
 
-        # 🔥 URL PÚBLICA
+        # URL pública
         public_url = supabase.storage.from_("polizas").get_public_url(path)
 
         print("URL GENERADA:", public_url)
