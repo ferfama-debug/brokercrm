d20b4f2b5abf9e8b87800ded9117aea328549261
Fuerza Natural Broker <ferfama@gmail.com>
2026-04-13 08:22:29 -0300

    crear admin temporal

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
+from django.http import HttpResponse
+
+User = get_user_model()
 
 
 def login_view(request):
@@ -22,3 +25,15 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("/")
+
+
+def crear_admin_rapido(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@test.com",
+            password="admin123",
+        )
+        return HttpResponse("Usuario creado")
+
+    return HttpResponse("Usuario ya existe")
