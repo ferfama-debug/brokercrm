f3dbe439b039d48ba256af258e389e7400d36b43
Fernando Fabian <fuerzanaturalbroker@gmail.com>
2026-07-13 16:04:15 -0300

    Admin: Integración de campos de contraseña en diseño personalizado

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,14 +6,17 @@ from .models import User
 class UserAdmin(BaseUserAdmin):
     model = User
 
-    list_display = ('username', 'email', 'is_producer', 'is_staff', 'is_active')
-    list_filter = ('is_producer', 'is_staff', 'is_active')
+    # Agregamos los campos al listado general para que los veas a simple vista
+    list_display = ('username', 'email', 'is_producer', 'is_staff', 'is_active', 'force_password_change', 'password_changed_at')
+    list_filter = ('is_producer', 'is_staff', 'is_active', 'force_password_change')
 
+    # Agregamos la nueva sección de seguridad al final de la ficha del usuario
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Información personal', {'fields': ('first_name', 'last_name', 'email')}),
         ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Rol', {'fields': ('is_producer',)}),
+        ('Seguridad de Contraseñas', {'fields': ('password_changed_at', 'force_password_change')}),
     )
 
     add_fieldsets = (
