cb2fe0ca780b328ccdc52a23bd6d1c0aec4bcb5f
ferfama-debug <ferfama@gmail.com>
2026-07-25 20:32:20 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # 👈 1. Importamos 'include'
 from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home),
+    
+    # 🟢 2. Agregamos esta línea para conectar la app de pólizas
+    path("polizas/", include("policies.urls")),
 ]
